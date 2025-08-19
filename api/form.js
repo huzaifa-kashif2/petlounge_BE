6ee8cdf1fe8cef0d5392dbf028ea2724ac12ffa8
huzaifa-kashif2 <huzaifa.kashif2@gmail.com>
2025-08-19 14:02:40 +0500

    Changes Made

--- api/form.js
+++ api/form.js
@@ -1,8 +1,9 @@
+import Form from "../models/formSchema.js";
 import twilio from "twilio";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ success: false, msg: "Method not allowed" });
+    return res.status(405).json({ success: false, msg: "Method Not Allowed" });
   }
 
   try {
@@ -25,23 +26,26 @@ export default async function handler(req, res) {
     const client = twilio(SID, AUTH);
 
     console.log("Received form data:", req.body);
+    console.log(SID, AUTH);
+
+    const form = new Form({
+      name,
+      phone,
+      email,
+      date,
+      timeHour,
+      timeMin,
+      timeAMPM,
+      numPets,
+      petType,
+      petName,
+      message,
+    });
+    await form.save();
 
     await client.messages.create({
       from: "whatsapp:+14155238886",
       to: "whatsapp:+923068748112",
-      body: `🐾 New Appointment Request:
-Name: ${name}
-Phone: ${phone}
-Email: ${email}
-Date: ${date} ${timeHour}:${timeMin} ${timeAMPM}
-Pets: ${numPets} ${petType}(s)
-Pet Name: ${petName || "N/A"}
-Message: ${message || "N/A"}`
-    });
-
-    await client.messages.create({
-      from: "whatsapp:+14155238886",
-      to: "whatsapp:+923237560007",
       body: `🐾 New Appointment Request:
 Name: ${name}
 Phone: ${phone}
